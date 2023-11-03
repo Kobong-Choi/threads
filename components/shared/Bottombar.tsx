f27d6f727b11f6c7559fa6a0a5757766480d5dfd
David <morabongs@naver.com>
2023-11-03 17:38:56 +0900

    Adding Profile

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+"use client";
+
+import { sidebarLinks } from "@/constants";
+import { usePathname, useRouter } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
 
 const Bottombar = () => {
-  return (
-    <h1>Bottombar</h1>
-  )
-}
+  const pathname = usePathname();
 
-export default Bottombar
+  return (
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`bottombar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-subtle-medium text-light-1 max-sm:hidden">
+                {/* 문자열을 공백을 기준으로 분할하고, 그 중에서 첫 번째 단어를 선택하는 것 */}
+                {link.label.split(/\s+/)[0]}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Bottombar;
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,8 +1,7 @@
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
-import { Organization } from "@clerk/nextjs/server";
+import { dark } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 
 const Topbar = () => {
   return (
@@ -29,6 +28,7 @@ const Topbar = () => {
         </div>
         <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4",
             },
